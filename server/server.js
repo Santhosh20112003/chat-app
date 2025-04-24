f6bc447bce0e351a708804e9886b092f103eefe2
PuviGS <puvigs007@gmail.com>
2025-04-24 23:34:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", 
+    origin: "https://chat-application-puvi-7bzvx8ikz-puviyarasu-g-ss-projects.vercel.app/", 
     methods: ["GET", "POST"],
   },
 });
