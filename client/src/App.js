89b3d1306ea70168a3437d661b6756ee26717099
PuviGS <puvigs007@gmail.com>
2025-04-27 18:56:46 +0530

    Updated frontend and backend links after deployment

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css'; // Tailwind CSS should be used instead in real projects
 
-const socket = io('http://localhost:4000');
+const socket = io('https://chat-app-uoqs.onrender.com');
 
 function App() {
   const [message, setMessage] = useState('');
