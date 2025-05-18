5509980023fa44e8e90375b3f5950bb3beb6c8b4
PuviGS <puvigs007@gmail.com>
2025-05-18 13:27:42 +0530

    full commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-39x6.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
